a84219756305c38eeffc69b39b3e8976fcc86de3
Jonathan Reeve <j_reeve@apple.com>
2025-06-24 16:42:52 -0700

    More spacy fixes

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "A dev environment for Spacy-layout";
 
   inputs = {
-    nixpkgs.url = "github.com/nixos/nixpkgs/nixos-unstable";
-    flake-utils.url = "github.com/numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
